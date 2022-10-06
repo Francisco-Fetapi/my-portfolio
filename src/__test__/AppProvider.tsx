5a443002a128df6f7852556dcc5327f4fbb10cb7
Francisco Fetapi <franciscofetapi10@gmail.com>
2022-10-06 13:59:08 +0100

    refactor: delete comments

--- src/__test__/AppProvider.tsx
+++ src/__test__/AppProvider.tsx
@@ -5,8 +5,6 @@ import { resetAllState } from "../store/App.store";
 import { ColorSchemeProvider } from "@mantine/core";
 
 import I18nProvider from "next-translate/I18nProvider";
-// import useTranslation from "next-translate/useTranslation";
-
 import common from "../../locales/en/common.json";
 
 interface AppSetupProps {
@@ -15,8 +13,6 @@ interface AppSetupProps {
 
 // for testing
 export function AppProvider({ children }: AppSetupProps) {
-  // const { lang } = useTranslation();
-
   return (
     <I18nProvider lang="en" namespaces={{ common }}>
       <Provider store={store}>
