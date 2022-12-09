8e88c15030f5d9902a39e8f47a61a726fcf7358c
Francisco Fetapi <franciscofetapi10@gmail.com>
2022-12-09 06:21:35 +0100

    feat: add a main title and secondary content at journey page

--- src/pages/journey.tsx
+++ src/pages/journey.tsx
@@ -1,14 +1,28 @@
 import Head from "next/head";
 import AppScheme from "../components/AppScheme";
+import { Box, Text } from "@mantine/core";
+import MainTitle from "../components/MainTitle";
+import { useStyles as useStylesHeroTitleComponent } from "../components/HeroTitle";
 
 export default function IndexPage() {
+  const { classes } = useStylesHeroTitleComponent();
   return (
     <div>
       <Head>
         <title>Jornada</title>
       </Head>
       <AppScheme>
-        <h1>Jornada</h1>
+        <Box mt={10}>
+          <MainTitle>Jornada</MainTitle>
+          <Box sx={{ zoom: 0.9, width: "100%", maxWidth: 800 }}>
+            <Text color="dimmed" className={classes.description}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste,
+              odio itaque enim tempora suscipit omnis quibusdam veniam amet
+              alias culpa error nemo numquam eveniet libero, perspiciatis, ex
+              aspernatur praesentium. At!
+            </Text>
+          </Box>
+        </Box>
       </AppScheme>
     </div>
   );
