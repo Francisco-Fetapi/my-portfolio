b67b9eb37c9edffc6733838228316134e28cd96e
Francisco Fetapi <franciscofetapi10@gmail.com>
2022-12-30 18:02:10 +0100

    feat: about page translated

--- locales/en/about.ts
+++ locales/en/about.ts
@@ -2,55 +2,53 @@ export type TranslationAbout = typeof translations;
 
 const translations = {
   auto_biography: `
-      Meu nome é <b>{{myName}}</b>, sou
+      My name is <b>{{myName}}</b>, I'm a
           <linkFullStack>{{myProfessional}}</linkFullStack>
-          que atua essencialmente com tecnologias do ecossistema
-          <linkJs>JavaScript</linkJs>. O meu
-          primeiro contato com programação foi em
-          <link2018>2018</link2018> durante o
-          <b>Ensino Médio</b>, nessa fase aprendi
+          that essentially works with ecosystem technologies
+          <linkJs>JavaScript</linkJs>. My first contact with programming was in
+          <link2018>2018</link2018> During
+          <b>middle teaching</b>, at this stage I learned
           <linkLogic>
-            Lógica de Programação
+          Programming Logic
           </linkLogic>
-          usando o <linkVisualG>Visual G</linkVisualG>.
-          Depois de criar
-          <linkAlgorithms> algoritmos</linkAlgorithms> que
-          eram executados no
-          <linkTerminal>terminal</linkTerminal> me vi curioso
-          em criar uma interface amigavél para os meus mini-programas, e foi
-          assim que me deparei com
+          using the <linkVisualG>Visual G</linkVisualG>.
+          After creating
+          <linkAlgorithms> algorithms</linkAlgorithms> than
+          were executed in the
+          <linkTerminal>prompt</linkTerminal> i saw myself curious
+          in creating a friendly interface for my mini-programs, and it was
+          so I came across
           <linkHTML>HTML5</linkHTML>,
-          <linkCSS>CSS3</linkCSS> e
-          <linkJs>JavaScript</linkJs>, e
-          desde ai continuei estudando sem parar.
+          <linkCSS>CSS3</linkCSS> and
+          <linkJs>JavaScript</linkJs>, and
+          since then I continued studying non-stop.
   `,
   auto_biography_2: `
-  Nasci aos
+  I was born at
           <b>
             {{birthday}} ({{years}} anos)
           </b>
-          , falo Português (minha língua materna) e tenho um Inglês
-          intermediário. Atualmente vivo em
+          , I speak Portuguese (my mother tongue) and have an intermediate English. I currently live in
           <linkLobito>
             Angola, Benguela, Lobito
           </linkLobito>.
   `,
   auto_biography_3: `
-  Comecei a trabalhar profissionalmente com programação atendendo
-          demandas de clientes da minha localidade, antes disso apenas criava
-          projetos de estudo.
+  I started working professionally with programming attending
+          demands of customers in my locality, before that only created
+          study projects.
   `,
-  auto_biography_4: `As minhas paixões são: Programação, Cinema (Filmes, Séries e Animes),
-  Música e Literatura.`,
-  about_me: "Sobre Mim",
+  auto_biography_4: `My passions are: Programming, Cinema (Movies, Series and Animes),
+  Music and Literature.`,
+  about_me: "About Me",
   intro_biography: `
-  Sou um cara apaixonado por desenvolvimento de software.
-                  Atualmente com foco em
+  I'm a guy in love with software development.
+                  Currently focusing on
                   <linkFullStack>
-                    Desenvolvimento Web full-stack
+                  Full-stack Web Development
                   </linkFullStack>.
   `,
-  to_contact: "Contactar",
+  to_contact: "Contact",
 };
 
 export default translations;
