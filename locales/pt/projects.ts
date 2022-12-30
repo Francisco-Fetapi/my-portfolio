b5afa637208db1fd0b0768fcda762708f4d3aade
Francisco Fetapi <franciscofetapi10@gmail.com>
2022-12-30 17:35:53 +0100

    feat: translating projects

--- locales/pt/projects.ts
+++ locales/pt/projects.ts
@@ -1,5 +1,21 @@
 const translations = {
   test: "teste",
+  intro: `
+  Desde o momento que digitei a minha primeira
+                  <i>linha de código</i> até agora desenvolvi vários projetos de
+                  estudo para ganhar proeficiencia nas tecnologias que conheço,
+                  além é claro de ter atendido as demandas de um ou outro
+                  cliente.
+                  <br></br>
+                  <br></br>
+                  Abaixo eis uma lista dos projetos que desenvolvi ao longo dos
+                  anos.
+  `,
+  top_projects: "EM DESTAQUE",
+  more_projects: "MAIS PROJETOS",
+  have_a_project_in_mind: "ALGUM PROJETO EM MENTE? VAMOS TRABALHAR JUNTOS",
+  lets_talk: "Vamos falar",
+  take_a_look_at_all_projects: "VER TODOS OS PROJETOS",
 };
 
 export default translations;
