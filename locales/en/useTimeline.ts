8a4ea0c6eec1c233b406399548dc09f223c6a2cb
Francisco Fetapi <franciscofetapi10@gmail.com>
2023-01-03 15:31:03 +0100

    feat: setup to translate useTimeline hook

---
+++ locales/en/useTimeline.ts
@@ -0,0 +1,5 @@
+export type TranslationUseTimeline = typeof translations;
+
+const translations = {};
+
+export default translations;
---
+++ locales/pt/useTimeline.ts
@@ -0,0 +1,5 @@
+import { TranslationUseTimeline } from "../en/useTimeline";
+
+const translations: TranslationUseTimeline = {};
+
+export default translations;
