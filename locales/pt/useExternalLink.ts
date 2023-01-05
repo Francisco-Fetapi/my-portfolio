c399819a6b7c169b38be8db0446b764ab7ef1f05
Francisco Fetapi <franciscofetapi10@gmail.com>
2023-01-05 09:49:14 +0100

    feat: translating external links

---
+++ locales/pt/useExternalLink.ts
@@ -0,0 +1,51 @@
+import { TranslationUseExternalLink } from "../en/useExternalLink";
+
+const translations: TranslationUseExternalLink = {
+  javascript: "https://pt.wikipedia.org/wiki/JavaScript",
+  html5: "https://pt.wikipedia.org/wiki/HTML",
+  css3: "https://pt.wikipedia.org/wiki/Cascading_Style_Sheets",
+  programmingLogic:
+    "https://pt.wikipedia.org/wiki/Programa%C3%A7%C3%A3o_l%C3%B3gica",
+  visualg: "https://pt.wikipedia.org/wiki/Visualg",
+  fullStack: "https://pt.frwiki.wiki/wiki/D%C3%A9veloppeur_full_stack",
+  algorithms: "https://pt.wikipedia.org/wiki/Algoritmo",
+  prompt: "https://pt.wikipedia.org/wiki/Terminal_(inform%C3%A1tica)",
+  lobito: "https://pt.wikipedia.org/wiki/Lobito",
+  cSharp: "https://pt.wikipedia.org/wiki/C_Sharp",
+  graphicInterface:
+    "https://pt.wikipedia.org/wiki/Interface_gr%C3%A1fica_do_utilizador",
+  php: "https://pt.wikipedia.org/wiki/PHP",
+  frontendAndbackend:
+    "https://www.alura.com.br/artigos/o-que-e-front-end-e-back-end",
+  jquery: "https://pt.wikipedia.org/wiki/JQuery",
+  gridlayout:
+    "https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Grid_Layout",
+  flexbox:
+    "https://developer.mozilla.org/pt-BR/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox",
+  buildingLayouts: "https://github.com/Francisco-Fetapi/construindo_layouts",
+  typescript: "https://pt.wikipedia.org/wiki/TypeScript",
+  mysql: "https://pt.wikipedia.org/wiki/MySQL",
+  crud: "https://pt.wikipedia.org/wiki/CRUD",
+  git: "https://pt.wikipedia.org/wiki/Git",
+  reactjs: "https://pt.wikipedia.org/wiki/React_(JavaScript)",
+  jsdefinitiveguide:
+    "https://www.pdfdrive.com/javascript-o-guia-definitivo-e185837261.html",
+  poo: "https://pt.wikipedia.org/wiki/Programa%C3%A7%C3%A3o_orientada_a_objetos",
+  modelbd: "https://pt.wikipedia.org/wiki/Modelagem_de_dados",
+  sql: "https://pt.wikipedia.org/wiki/SQL",
+  laravel: "https://pt.wikipedia.org/wiki/Laravel",
+  typescriptdocs: "https://www.typescriptlang.org/pt/docs/handbook/",
+  designpatterns:
+    "https://pt.wikipedia.org/wiki/Padr%C3%A3o_de_projeto_de_software",
+  testing: "https://pt.wikipedia.org/wiki/Teste_de_software",
+  nextjs: "https://pt.wikipedia.org/wiki/Next.js",
+  nextjsdocs: "https://nextjs.org/docs/getting-started",
+  tmdb: "https://pt.wikipedia.org/wiki/The_Movie_Database",
+  redux: "https://pt.wikipedia.org/wiki/Redux_(biblioteca_JavaScript)",
+  reduxtoolkit: "https://redux-toolkit.js.org/",
+  tictactoe: "https://pt.wikipedia.org/wiki/Jogo_da_velha",
+  quemquerserricoapp: "https://quem-quer-ser-rico-lite.br.uptodown.com/android",
+  datilography: "https://pt.wikipedia.org/wiki/Datilografia",
+};
+
+export default translations;
--- src/hooks/useExternalLink.ts
+++ src/hooks/useExternalLink.ts
@@ -1,96 +1,113 @@
+import useTranslation from "next-translate/useTranslation";
+import { useMemo } from "react";
+import { TranslationUseExternalLink } from "../../locales/en/useExternalLink";
 import useMe from "../database/useMe";
 
 export type IExternalLinks = ReturnType<typeof useExternalLink>;
+type LinksToTranslate = (keyof TranslationUseExternalLink)[];
+
+const linksToTranslate: LinksToTranslate = [
+  "algorithms",
+  "buildingLayouts",
+  "cSharp",
+  "crud",
+  "css3",
+  "datilography",
+  "designpatterns",
+  "flexbox",
+  "frontendAndbackend",
+  "fullStack",
+  "git",
+  "graphicInterface",
+  "gridlayout",
+  "html5",
+  "javascript",
+  "jquery",
+  "jsdefinitiveguide",
+  "laravel",
+  "lobito",
+  "modelbd",
+  "mysql",
+  "nextjs",
+  "nextjsdocs",
+  "php",
+  "poo",
+  "programmingLogic",
+  "prompt",
+  "quemquerserricoapp",
+  "reactjs",
+  "redux",
+  "reduxtoolkit",
+  "sql",
+  "testing",
+  "tictactoe",
+  "tmdb",
+  "typescript",
+  "typescriptdocs",
+  "visualg",
+];
 
 export default function useExternalLink() {
   const { getRepositoryByName } = useMe();
-  return {
-    javascript: "https://pt.wikipedia.org/wiki/JavaScript",
-    html5: "https://pt.wikipedia.org/wiki/HTML",
-    css3: "https://pt.wikipedia.org/wiki/Cascading_Style_Sheets",
-    programmingLogic:
-      "https://pt.wikipedia.org/wiki/Programa%C3%A7%C3%A3o_l%C3%B3gica",
-    visualg: "https://pt.wikipedia.org/wiki/Visualg",
-    fullStack: "https://pt.frwiki.wiki/wiki/D%C3%A9veloppeur_full_stack",
-    algorithms: "https://pt.wikipedia.org/wiki/Algoritmo",
-    prompt: "https://pt.wikipedia.org/wiki/Terminal_(inform%C3%A1tica)",
-    lobito: "https://pt.wikipedia.org/wiki/Lobito",
-    algorithmCourse:
-      "https://www.youtube.com/playlist?list=PLHz_AreHm4dmSj0MHol_aoNYCSGFqvfXV",
-    cSharp: "https://pt.wikipedia.org/wiki/C_Sharp",
-    courseHTMLCSSiConectado: "https://www.youtube.com/watch?v=JMWutVBmZbc",
-    graphicInterface:
-      "https://pt.wikipedia.org/wiki/Interface_gr%C3%A1fica_do_utilizador",
-    courseJSEssencial:
-      "https://www.youtube.com/watch?v=ipHuSfOYhwA&list=PLInBAd9OZCzxl38aAYdyoMHVg0xCgxrRx",
-    rbteachChannel: "https://www.youtube.com/@RBtechinfo",
-    htmlcsscaelumlivro:
-      "https://www.caelum.com.br/apostila/apostila-html-css-javascript.pdf",
-    php: "https://pt.wikipedia.org/wiki/PHP",
-    frontendAndbackend: "https://en.wikipedia.org/wiki/Frontend_and_backend",
-    jquery: "https://pt.wikipedia.org/wiki/JQuery",
-    htmlcssk19livro:
-      "https://pt.scribd.com/doc/171072822/k19-k02-Desenvolvimento-Web-Com-HTML-Css-Javascript",
-    gridlayout:
-      "https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Grid_Layout",
-    flexbox:
-      "https://developer.mozilla.org/pt-BR/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox",
+  const { t } = useTranslation("useExternalLink");
+
+  const firstLinks = {
+    socialmoviesspace: getRepositoryByName("projeto-tcc"),
+    courselaraveljoao:
+      "https://www.youtube.com/watch?v=0T5gM1WRNsY&list=PLXik_5Br-zO893qVjjP7a4qg4NYrl33w1",
+    coursemysqlcursoemvideo:
+      "https://www.youtube.com/watch?v=Ofktsne-utM&list=PLHz_AreHm4dkBs-795Dsgvau_ekxg8g1r",
+    coursejoaoreact:
+      "https://www.youtube.com/watch?v=C8M94QLJy0o&list=PLXik_5Br-zO9YVs9bxi7zoQlKq59VPTX1",
+    bootstrap: "https://getbootstrap.com/",
+    reactjsdocs: "https://reactjs.org/docs/getting-started.html",
+    mui: "https://mui.com/",
+    github: "https://github.com",
+    phpcourseexcript:
+      "https://www.youtube.com/watch?v=R_yRrYUHai0&list=PLesCEcYj003TrV2MvUOnmVtMdgIp0C4Pd",
+    phpcoursecursoemvideo:
+      "https://www.youtube.com/watch?v=F7KzJ7e6EAc&list=PLHz_AreHm4dm4beCCCmW4xwpmLf6EHY9k",
+    phppoocursoemvideo:
+      "https://www.youtube.com/watch?v=KlIL63MeyMY&list=PLHz_AreHm4dmGuLII3tsvryMMD7VgcT7x",
+    mirrorFashion: getRepositoryByName("Site-Mirror-fashion"),
+    exercices2019:
+      "https://github.com/Francisco-Fetapi/primeiros_exercicios_2019",
     cursohtmlcsscursoemvideo:
       "https://www.youtube.com/playlist?list=PLHz_AreHm4dlAnJ_jJtV29RFxnPHDuk9o",
     cursojscursoemvideo:
       "https://www.youtube.com/playlist?list=PLntvgXM11X6pi7mW0O4ZmfUI1xDSIbmTm",
     exercicesjs:
       "https://github.com/Francisco-Fetapi/JavaScript_exercicios-cursoemvideo",
-    exercices2019:
-      "https://github.com/Francisco-Fetapi/primeiros_exercicios_2019",
-    buildingLayouts: "https://github.com/Francisco-Fetapi/construindo_layouts",
-    mirrorFashion: "https://github.com/Francisco-Fetapi/Site-Mirror-fashion",
-    typescript: "https://pt.wikipedia.org/wiki/TypeScript",
-    phpcoursecursoemvideo:
-      "https://www.youtube.com/watch?v=F7KzJ7e6EAc&list=PLHz_AreHm4dm4beCCCmW4xwpmLf6EHY9k",
-    phppoocursoemvideo:
-      "https://www.youtube.com/watch?v=KlIL63MeyMY&list=PLHz_AreHm4dmGuLII3tsvryMMD7VgcT7x",
-    mysql: "https://pt.wikipedia.org/wiki/MySQL",
-    crud: "https://pt.wikipedia.org/wiki/CRUD",
-    phpcourseexcript:
-      "https://www.youtube.com/watch?v=R_yRrYUHai0&list=PLesCEcYj003TrV2MvUOnmVtMdgIp0C4Pd",
-    git: "https://pt.wikipedia.org/wiki/Git",
-    github: "https://github.com",
-    reactjs: "https://pt.wikipedia.org/wiki/React_(JavaScript)",
-    reactjsdocs: "https://reactjs.org/docs/getting-started.html",
-    mui: "https://mui.com/",
-    bootstrap: "https://getbootstrap.com/",
-    coursejoaoreact:
-      "https://www.youtube.com/watch?v=C8M94QLJy0o&list=PLXik_5Br-zO9YVs9bxi7zoQlKq59VPTX1",
-    jsdefinitiveguide:
-      "https://www.pdfdrive.com/javascript-o-guia-definitivo-e185837261.html",
-    poo: "https://pt.wikipedia.org/wiki/Programa%C3%A7%C3%A3o_orientada_a_objetos",
-    coursemysqlcursoemvideo:
-      "https://www.youtube.com/watch?v=Ofktsne-utM&list=PLHz_AreHm4dkBs-795Dsgvau_ekxg8g1r",
-    modelbd: "https://pt.wikipedia.org/wiki/Modelagem_de_dados",
-    sql: "https://pt.wikipedia.org/wiki/SQL",
-    courselaraveljoao:
-      "https://www.youtube.com/watch?v=0T5gM1WRNsY&list=PLXik_5Br-zO893qVjjP7a4qg4NYrl33w1",
-    laravel: "https://pt.wikipedia.org/wiki/Laravel",
-    socialmoviesspace: getRepositoryByName("projeto-tcc"),
+    htmlcssk19livro:
+      "https://pt.scribd.com/doc/171072822/k19-k02-Desenvolvimento-Web-Com-HTML-Css-Javascript",
+    htmlcsscaelumlivro:
+      "https://www.caelum.com.br/apostila/apostila-html-css-javascript.pdf",
+    courseJSEssencial:
+      "https://www.youtube.com/watch?v=ipHuSfOYhwA&list=PLInBAd9OZCzxl38aAYdyoMHVg0xCgxrRx",
+    rbteachChannel: "https://www.youtube.com/@RBtechinfo",
+    courseHTMLCSSiConectado: "https://www.youtube.com/watch?v=JMWutVBmZbc",
+    algorithmCourse:
+      "https://www.youtube.com/playlist?list=PLHz_AreHm4dmSj0MHol_aoNYCSGFqvfXV",
     tccpdf:
       "https://drive.google.com/file/d/1PUuUInKajHslJYbmJ8yJGYgI7gmkGLvi/view?usp=share_link",
-    typescriptdocs: "https://www.typescriptlang.org/pt/docs/handbook/",
     coursetypescriptotavio:
       "https://www.youtube.com/watch?v=oQPYxhqoH1o&list=PLbIBj8vQhvm0Q_TMvL22kkpdUW8sP8Yql",
-    designpatterns:
-      "https://pt.wikipedia.org/wiki/Padr%C3%A3o_de_projeto_de_software",
-    testing: "https://pt.wikipedia.org/wiki/Teste_de_software",
-    nextjs: "https://pt.wikipedia.org/wiki/Next.js",
-    nextjsdocs: "https://nextjs.org/docs/getting-started",
-    tmdb: "https://pt.wikipedia.org/wiki/The_Movie_Database",
-    redux: "https://pt.wikipedia.org/wiki/Redux_(biblioteca_JavaScript)",
-    reduxtoolkit: "https://redux-toolkit.js.org/",
-    tictactoe: "https://pt.wikipedia.org/wiki/Jogo_da_velha",
     souangolanoeconhecoangolaapp:
       "https://play.google.com/store/apps/details?id=ao.evolium.conhecoangola&hl=pt&gl=US",
-    quemquerserricoapp:
-      "https://quem-quer-ser-rico-lite.br.uptodown.com/android",
-    datilography: "https://pt.wikipedia.org/wiki/Datilografia",
+  };
+
+  const linksTranslated = useMemo(() => {
+    const links: Partial<TranslationUseExternalLink> = {};
+    linksToTranslate.forEach((link) => {
+      links[link] = t(link);
+    });
+
+    console.log(links);
+    return links;
+  }, []);
+
+  return {
+    ...firstLinks,
+    ...linksTranslated,
   };
 }
