73831917e53c8a96594d338aa8a6a3aa8ba89b3e
Francisco Fetapi <franciscofetapi10@gmail.com>
2023-01-06 14:32:04 +0100

    refactor: about translating

--- locales/en/about.ts
+++ locales/en/about.ts
@@ -1,14 +1,14 @@
 export type TranslationAbout = typeof translations;
 
 const translations = {
-  seo_description: `My name is Francisco Fetapi, I'm a Full-Stack Developer that essentially works with ecosystem technologies JavaScript. My first contact with programming was in 2018 During middle teaching, at this stage I learned Programming Logic using the Visual G. After creating algorithms than were executed in the prompt i saw myself curious in creating a friendly interface for my mini-programs, and it was so I came across HTML5, CSS3 and JavaScript, and since then I continued studying non-stop.`,
+  seo_description: `My name is Francisco Fetapi, I'm a Full-Stack Developer that essentially works with ecosystem technologies JavaScript. My first contact with programming was in 2018 During high school, at this stage I learned Programming Logic using the Visual G. After creating algorithms than were executed in the prompt i saw myself curious in creating a friendly interface for my mini-programs, and it was so I came across HTML5, CSS3 and JavaScript, and since then I continued studying non-stop.`,
   auto_biography: `
       My name is <b>{{myName}}</b>, I'm a
           <linkFullStack>{{myProfessional}}</linkFullStack>
           that essentially works with ecosystem technologies
           <linkJs>JavaScript</linkJs>. My first contact with programming was in
           <link2018>2018</link2018> During
-          <b>middle teaching</b>, at this stage I learned
+          <b>high school</b>, at this stage I learned
           <linkLogic>
           Programming Logic
           </linkLogic>
